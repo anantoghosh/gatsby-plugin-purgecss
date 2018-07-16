dd56f4f4310bfe42fbfc70666090dbd0b89875f2
Ananto Ghosh <a0ghosh@gmail.com>
2018-07-16 22:11:04 +0530

    build((ci, test)): Simplify npm scripts with runjs

---
+++ runfile.js
@@ -0,0 +1,72 @@
+const { run } = require('runjs');
+const fs = require('fs-extra');
+const symlinkDir = require('symlink-dir');
+
+const c = {
+  build: 'babel src --out-dir .',
+  buildTest: 'babel src --out-dir plugins/gatsby-plugin-purgecss',
+  pack: 'npm pack',
+  buildTestBuild: 'cd test_build && npm run build && cd ..',
+  jestCoverage: 'jest --coverage',
+  jestUnit: 'jest unit',
+  jestE2E: 'jest build',
+  install(filename) {
+    return `cd test_build && yarn add -D file:/../${filename}`;
+  }
+};
+
+function test_ci() {
+  console.log('\nBuilding');
+  run(c.build);
+
+  console.log('\nCreating npm package');
+  run(c.pack);
+
+  console.log('\nRunning gatsby build in test_build');
+  run(c.buildTestBuild);
+
+  console.log('\nInstalling gatsby-plugin-purgecss package');
+  let files = fs.readdirSync('.', 'utf8');
+  files = files.filter(file => {
+    return file.includes('gatsby-plugin-purgecss');
+  });
+  run(c.install(files[0]));
+
+  console.log('\nRunning tests with coverage');
+  run(c.jestCoverage);
+}
+
+function test() {
+  console.log('\nRunning unit tests');
+  run(c.jestUnit);
+
+  console.log('\nBuilding to plugins folder');
+  run(c.buildTest);
+
+  console.log('\nMaking an empty package.json for plugin');
+  try {
+    fs.copySync('./empty.json', 'plugins/gatsby-plugin-purgecss/package.json');
+    console.log('\nSuccess');
+  } catch (err) {
+    console.error(err);
+  }
+
+  console.log('\nLinking node_modules and plugins folder to test_build');
+  symlinkDir('node_modules', 'test_build/node_modules/')
+    .then(result => symlinkDir('plugins', 'test_build/plugins/'))
+    .then(result => {
+      console.log('\nSuccess');
+
+      console.log('\nRunning gatsby build in test_build');
+      run(c.buildTestBuild);
+
+      console.log('\nRunning e2e tests');
+      run(c.jestE2E);
+    })
+    .catch(err => console.error(err));
+}
+
+module.exports = {
+  test_ci,
+  test
+};
