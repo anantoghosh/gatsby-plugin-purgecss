5a800f38c5125d3b17949e85d71c44f8ecec7e51
Ananto Ghosh <a0ghosh@gmail.com>
2018-08-14 17:56:05 +0530

    test: Add scss to index

--- test_build/src/pages/index.js
+++ test_build/src/pages/index.js
@@ -6,6 +6,8 @@ import stylus from './stylus.styl';
 import stylusModule from './stylus.module.styl';
 import sass from './sass.sass';
 import sassModule from './sass.module.sass';
+import scss from './scss.scss';
+import scssModule from './scss.module.scss';
 import less from './less.less';
 import lessModule from './less.module.less';
 
@@ -17,6 +19,8 @@ export default () => (
     <div className={stylusModule.stylusModuleOk} />
     <div className={sass.sassOk} />
     <div className={sassModule.sassModuleOk} />
+    <div className={scss.scssOk} />
+    <div className={scssModule.scssModuleOk} />
     <div className={less.lessOk} />
     <div className={lessModule.lessModuleOk} />
   </div>
