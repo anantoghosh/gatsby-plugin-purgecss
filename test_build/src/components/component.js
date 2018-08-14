7c74f7c4337bb854adbe0d60deff6d3ad617a615
Ananto Ghosh <a0ghosh@gmail.com>
2018-08-14 17:55:11 +0530

    test: Removed unused test files

--- test_build/src/components/component.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import styles from './component.module.css';
-
-export default () => (
-  <div className={`${styles.ok1} ${styles.ok2} ${styles.ok3}`}>
-    <a href="#">link</a>
-  </div>
-);
