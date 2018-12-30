7dabe097a36e6fbe5efb7bae3a6a7657cc668f2b
Ananto Ghosh <a0ghosh@gmail.com>
2018-12-30 13:33:50 +0530

    test: Add Tailwindcss tests

---
+++ test_tailwind/src/pages/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './tailwind.css';
+
+export default () => (
+  <div>
+    <button class="bg-blue hover:bg-blue-dark text-white font-bold py-2 px-4 rounded">
+      Button
+    </button>
+  </div>
+);
