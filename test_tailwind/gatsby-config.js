be9793e9e8722d4ac7046feed08e1720e1c34d6f
Ananto Ghosh <a0ghosh@gmail.com>
2021-03-01 22:47:59 +0530

    test: add initial tailwind test folder

--- test_tailwind/gatsby-config.js
+++ test_tailwind/gatsby-config.js
@@ -1,14 +1,11 @@
 module.exports = {
   plugins: [
-    `gatsby-plugin-stylus`,
-    `gatsby-plugin-sass`,
-    `gatsby-plugin-less`,
     `gatsby-plugin-postcss`,
     {
       resolve: 'gatsby-plugin-purgecss',
       options: {
-        tailwind: true
-      }
-    }
-  ]
+        tailwind: true,
+      },
+    },
+  ],
 };
