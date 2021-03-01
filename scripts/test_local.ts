ec64e4800694db34553a8f39f5695f8059439ced
Ananto Ghosh <a0ghosh@gmail.com>
2021-03-02 02:03:12 +0530

    ci: return 1 on error

--- scripts/test_local.ts
+++ scripts/test_local.ts
@@ -5,15 +5,20 @@ import c = require("./commands");
 
 /** Main */
 (async () => {
-  await sh(c.jestUnit, "Unit Testing");
-  await sh(c.buildLocal, "Building");
+  try {
+    await sh(c.jestUnit, "Unit Testing");
+    await sh(c.buildLocal, "Building");
 
-  console.log("\nMaking an empty package.json for plugin");
-  fs.copySync("./empty.json", "plugins/gatsby-plugin-purgecss/package.json");
-  fs.copySync("plugins/", "test_build/plugins/");
+    console.log("\nMaking an empty package.json for plugin");
+    fs.copySync("./empty.json", "plugins/gatsby-plugin-purgecss/package.json");
+    fs.copySync("plugins/", "test_build/plugins/");
 
-  process.chdir("test_build");
-  await sh(c.buildTestBuild, "Build gatsby in test directory");
-  process.chdir("..");
-  await sh(c.jestE2E, "Run E2E tests");
+    process.chdir("test_build");
+    await sh(c.buildTestBuild, "Build gatsby in test directory");
+    process.chdir("..");
+    await sh(c.jestE2E, "Run E2E tests");
+  } catch (error) {
+    console.error(error);
+    return 1;
+  }
 })();
