be1f095239074ca626e1eedd13a9344dd97becc8
Ananto Ghosh <a0ghosh@gmail.com>
2021-03-03 23:16:02 +0530

    build: move tests to gatsby v3

--- test_build/src/pages/index.js
+++ test_build/src/pages/index.js
@@ -1,17 +1,17 @@
 import React from 'react';
 import './global.css';
-import css from './css.css';
-import cssModule from './css.module.css';
-import stylus from './stylus.styl';
-import stylusModule from './stylus.module.styl';
-import sass from './sass.sass';
-import sassModule from './sass.module.sass';
-import scss from './scss.scss';
-import scssModule from './scss.module.scss';
-import less from './less.less';
-import lessModule from './less.module.less';
+import './css.css';
+import * as cssModule from './css.module.css';
+import './stylus.styl';
+import * as stylusModule from './stylus.module.styl';
+import './sass.sass';
+import * as sassModule from './sass.module.sass';
+import './scss.scss';
+import * as scssModule from './scss.module.scss';
+import './less.less';
+import * as lessModule from './less.module.less';
 import './whitelisted.css';
-import other from './other.css';
+import './other.css';
 import './ignored.css';
 import './ignored/ignoredFolder.css';
 import '../pages2/style.css';
@@ -20,19 +20,19 @@ import '../pages3/style.css';
 
 export default () => (
   <div className="global-ok">
-    <div className={css.cssOk} />
+    <div className={'cssOk'} />
     <div className={cssModule.cssModuleOk} />
-    <div className={stylus.stylusOk} />
+    <div className={'stylusOk'} />
     <div className={stylusModule.stylusModuleOk} />
-    <div className={sass.sassOk} />
+    <div className={'sassOk'} />
     <div className={sassModule.sassModuleOk} />
-    <div className={scss.scssOk} />
+    <div className={'scssOk'} />
     <div className={scssModule.scssModuleOk} />
-    <div className={less.lessOk} />
+    <div className={'lessOk'} />
     <div className={lessModule.lessModuleOk} />
-    //btn-large
-    <div className={other.btnLarge} />
-    <div className={other.btn_small} />
-    <div className={other['btn-medium']} />
+    {/* btn-large */}
+    <div className={'btnLarge'} />
+    <div className={'btn_small'} />
+    <div className={'btn-medium'} />
   </div>
 );
